7e0ab2fee0c1f576a2bfecda1b870d1c38b41d04
Giang Pham <giangpham0508@gmail.com>
2016-10-24 16:49:43 +1100

    Change style a bit

--- resources/views/manage/ui_render/question_show.blade.php
+++ resources/views/manage/ui_render/question_show.blade.php
@@ -2,7 +2,7 @@
 <div class="questions_row row">
     <div data-question-type="single-choice" class="question_container col-lg-12 col-md-12 col-sm-12 col-xs-12" tabindex="10">
         <div id="question-field-{{$question->id}}" data-question-id="question{{$question->id}}">
-            <fieldset>
+            <fieldset style="margin-top: 2px">
                 <h4 id="question-title-{{$question->id}}">{{isset($question_number)?$question_number:'not'}}. {{$question->strQuestionTitle}}</h4>
                 <div class="question_body">
                     <?php $type = $question->question_types_id;?>
