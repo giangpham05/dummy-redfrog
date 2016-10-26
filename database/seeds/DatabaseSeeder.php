578388e39907df7e8dac0712527fe5768b9fed65
Giang Pham <giangpham0508@gmail.com>
2016-10-27 00:44:22 +1100

    Added Question Type seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,15 +15,14 @@ class DatabaseSeeder extends Seeder
         Eloquent::unguard();
 
         //disable foreign key check for this connection before running seeders
-//        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        $this->call(ClientsTableSeeder::class);
+        //$this->call(ClientsTableSeeder::class);
+        $this->call(QuestionTypesSeeder::class);
         //$this->call(SurveysTableSeeder::class);
         //$this->call(UsersSeeder::class);
 
-        // supposed to only apply to a single connection and reset it's self
-        // but I like to explicitly undo what I've done for clarity
-//        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
     }
 }
