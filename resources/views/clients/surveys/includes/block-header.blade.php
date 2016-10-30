386a57af46ba5b7935c3d53f6e72e6ea58dd8a5b
Giang Pham <giangpham0508@gmail.com>
2016-10-30 22:16:54 +1100

    All changed

---
+++ resources/views/clients/surveys/includes/block-header.blade.php
@@ -0,0 +1,6 @@
+<div class="block-header">
+    <div class="icon" style="padding-top: 5px;padding-bottom: 5px;display: inline-flex; vertical-align: middle;">
+        <i class="material-icons" style="margin-top: 4px;">{{$icon}}</i>
+        <span class="" style="padding: 8px 0 7px 3px;"><h2>{{$title}}</h2></span>
+    </div>
+</div>
